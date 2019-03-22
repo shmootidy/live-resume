acce11c6911cffa0dbd1176e65e457ec6f3d5fb8
Shmoo <shmooritchie@gmail.com>
2019-03-22 12:30:30 -0700

    Define text color in tabs.

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 
+import './style/tabs.css'
+
 export default class Tabs extends Component {
   render() {
     return (
-      <ul>
-        <li>Readme</li>
-        <li>Projects</li>
-        <li>Education</li>
-        <li>Experience</li>
+      <ul class="flex">
+        <li id="tab1">Readme</li>
+        <li id="tab2">Projects</li>
+        <li id="tab3">Education</li>
+        <li id="tab4">Experience</li>
       </ul>
     )
   }
