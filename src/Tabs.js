9f831f7cd105479acc5e09548f39f65cf7558096
Shmoo <shmooritchie@gmail.com>
2019-03-22 13:13:13 -0700

    Refactor styling.
    Move @font-face to top level css. Add wrapper to tabs to specify margin and eliminate default margin on ul elm. Tabs are sitting properly now.

--- src/Tabs.js
+++ src/Tabs.js
@@ -5,12 +5,14 @@ import './style/tabs.css'
 export default class Tabs extends Component {
   render() {
     return (
-      <ul className="flex">
-        <li className="tab1">Readme</li>
-        <li className="tab2 not-selected">Projects</li>
-        <li className="tab3 not-selected">Education</li>
-        <li className="tab4 not-selected">Experience</li>
-      </ul>
+      <div class="margin">
+        <ul className="flex">
+          <li className="tab1">Readme</li>
+          <li className="tab2 not-selected">Projects</li>
+          <li className="tab3 not-selected">Education</li>
+          <li className="tab4 not-selected">Experience</li>
+        </ul>
+      </div>
     )
   }
 }
