6e7f3e08132edb55316930b34bfc64bb20f1bf62
Shmoo <shmooritchie@gmail.com>
2019-03-22 12:22:35 -0700

    Populate Tabs component with text. Remove bottom padding from name-title.

--- src/Tabs.js
+++ src/Tabs.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 export default class Tabs extends Component {
   render() {
     return (
-      <div> tabs</div>
+      <ul>
+        <li>Readme</li>
+        <li>Projects</li>
+        <li>Education</li>
+        <li>Experience</li>
+      </ul>
     )
   }
 }
