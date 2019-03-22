110253028b85bff6aa85c891db4b93d9528a88d0
Shmoo <shmooritchie@gmail.com>
2019-03-22 12:46:44 -0700

    Correct JSX (class to className).

--- src/NameTitle.js
+++ src/NameTitle.js
@@ -5,9 +5,9 @@ export default class NameTitle extends Component {
     return (
       <span>
         <div id="initials">sr</div>
-        <span id="name-title" class="flex flex-column">
+        <span id="name-title" className="flex flex-column">
           <span id="name">shmoo-ritchie</span>
-          <span id="title">Full-stack <span class="green">Web</span> Developer</span>
+          <span id="title">Full-stack <span className="green">Web</span> Developer</span>
         </span>
       </span>
     )
--- src/Tabs.js
+++ src/Tabs.js
@@ -5,11 +5,11 @@ import './style/tabs.css'
 export default class Tabs extends Component {
   render() {
     return (
-      <ul class="flex">
-        <li class="tab1">Readme</li>
-        <li class="tab2 not-selected">Projects</li>
-        <li class="tab3 not-selected">Education</li>
-        <li class="tab4 not-selected">Experience</li>
+      <ul className="flex">
+        <li className="tab1">Readme</li>
+        <li className="tab2 not-selected">Projects</li>
+        <li className="tab3 not-selected">Education</li>
+        <li className="tab4 not-selected">Experience</li>
       </ul>
     )
   }
