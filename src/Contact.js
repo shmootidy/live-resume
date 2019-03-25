62294bc40c95c79b948c0e3e49571a31e1e0359e
Shmoo <shmooritchie@gmail.com>
2019-03-24 18:19:29 -0700

    Style contact to mimic npm.

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class Contact extends Component {
   render() {
     return (
-      <div>Contact</div>
+      <div id="contact">
+        <div className="sidebar-subtitle">contact</div>
+      </div>
     )
   }
 }
