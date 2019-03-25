59240b78360dada50442736afcacddc51a3f26e7
Shmoo <shmooritchie@gmail.com>
2019-03-24 18:37:32 -0700

    Style contact bars.

--- src/Contact.js
+++ src/Contact.js
@@ -5,6 +5,11 @@ export default class Contact extends Component {
     return (
       <div id="contact">
         <div className="sidebar-subtitle">contact</div>
+        <div className="contact-bar" id="phone">> 604.616.0247
+        </div>
+        <div className="contact-bar" id="email">> shmooritchie@gmail.com</div>
+        <div className="contact-bar" id="linked-in">> in/shmoo-ritchie</div>
+        <div className="contact-bar" id="github">> /shmootidy</div>
       </div>
     )
   }
