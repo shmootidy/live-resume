f3c7a850c10e0c9a09165e8d6738639776c23ae8
Shmoo <shmooritchie@gmail.com>
2019-03-24 17:22:37 -0700

    Revise summary text content.

--- src/Summary.js
+++ src/Summary.js
@@ -4,8 +4,8 @@ export default class Summary extends Component {
   render() {
     return (
       <div id="summary">
-        <h1 className='content-title'>shmoo-ritchie</h1>
-        <p>Self-motivated, full-stack web developer passionate about finding elegant solutions to complex problems. Inspired by collaboration, constraints, and deadlines. High level of integrity, creativity, and tenacity, honed by years as a teacher, musician and entrepreneur. Always keen to learn and improve.</p>
+        {/* <h1 className='content-title'>shmoo-ritchie</h1> */}
+        <p>Passionate about finding elegant solutions to complex problems. Inspired by collaboration, constraints, and deadlines. High level of integrity, creativity, and tenacity, honed by years as a teacher, musician and entrepreneur. Always keen to learn and improve.</p>
       </div>
     )
   }
