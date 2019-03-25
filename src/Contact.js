a46814f13dab7e4c1b6cb2edaa7c8b7c4d959898
Shmoo <shmooritchie@gmail.com>
2019-03-24 19:33:59 -0700

    Define links.

--- src/Contact.js
+++ src/Contact.js
@@ -5,19 +5,19 @@ export default class Contact extends Component {
     return (
       <div id="contact">
         <div className="sidebar-subtitle">contact</div>
-        <a href="#" className="contact-bar" id="phone">
+        <a href="tel:+1-604-616-0247" className="contact-bar" id="phone">
           <img className="contact-icon absolute" alt="Phone icon" src={require("./assets/phone.png")} />
           <span className="contact-info">> 604.616.0247</span>
         </a>
-        <a href="#" className="contact-bar" id="email">
+        <a href="mailto:shmooritchie@gmail.com" className="contact-bar" id="email">
           <img className="contact-icon absolute" alt="Email icon" src={require("./assets/email.png")} />
           <span className="contact-info">> shmooritchie@gmail.com</span>
         </a>
-        <a href="#" className="contact-bar" id="linked-in">
+        <a href="https://www.linkedin.com/in/shmoo-ritchie" target="_blank" className="contact-bar" id="linked-in">
           <img className="contact-icon absolute" alt="Linked-in icon" src={require("./assets/linked-in.png")} />
           <span className="contact-info">> in/shmoo-ritchie</span>
         </a>
-        <a href="#" className="contact-bar" id="github">
+        <a href="https://www.github.com/shmootidy" target="_blank" className="contact-bar" id="github">
           <img className="contact-icon absolute" alt="Github icon" src={require("./assets/github.png")} />
           <span className="contact-info">> /shmootidy</span>
         </a>
