1de1f200e1573bea0156d0ef4e4d6f51315ca043
Shmoo <shmooritchie@gmail.com>
2019-03-24 19:27:38 -0700

    Change contact divs to anchor tags. Style.

--- src/Contact.js
+++ src/Contact.js
@@ -5,22 +5,22 @@ export default class Contact extends Component {
     return (
       <div id="contact">
         <div className="sidebar-subtitle">contact</div>
-        <div className="contact-bar" id="phone">
+        <a href="#" className="contact-bar" id="phone">
           <img className="contact-icon absolute" alt="Phone icon" src={require("./assets/phone.png")} />
           <span className="contact-info">> 604.616.0247</span>
-        </div>
-        <div className="contact-bar" id="email">
+        </a>
+        <a href="#" className="contact-bar" id="email">
           <img className="contact-icon absolute" alt="Email icon" src={require("./assets/email.png")} />
           <span className="contact-info">> shmooritchie@gmail.com</span>
-        </div>
-        <div className="contact-bar" id="linked-in">
+        </a>
+        <a href="#" className="contact-bar" id="linked-in">
           <img className="contact-icon absolute" alt="Linked-in icon" src={require("./assets/linked-in.png")} />
           <span className="contact-info">> in/shmoo-ritchie</span>
-        </div>
-        <div className="contact-bar" id="github">
+        </a>
+        <a href="#" className="contact-bar" id="github">
           <img className="contact-icon absolute" alt="Github icon" src={require("./assets/github.png")} />
           <span className="contact-info">> /shmootidy</span>
-        </div>
+        </a>
       </div>
     )
   }
