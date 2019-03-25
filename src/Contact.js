56da4b5e37e2e7e3b8fb3d5bcc9d758380588050
Shmoo <shmooritchie@gmail.com>
2019-03-24 19:46:34 -0700

    Add rel to _blank anchors as per warning instructions.

--- src/Contact.js
+++ src/Contact.js
@@ -13,11 +13,11 @@ export default class Contact extends Component {
           <img className="contact-icon absolute" alt="Email icon" src={require("./assets/email.png")} />
           <span className="contact-info">> shmooritchie@gmail.com</span>
         </a>
-        <a href="https://www.linkedin.com/in/shmoo-ritchie" target="_blank" className="contact-bar" id="linked-in">
+        <a href="https://www.linkedin.com/in/shmoo-ritchie" target="_blank" rel="noopener noreferrer" className="contact-bar" id="linked-in">
           <img className="contact-icon absolute" alt="Linked-in icon" src={require("./assets/linked-in.png")} />
           <span className="contact-info">> in/shmoo-ritchie</span>
         </a>
-        <a href="https://www.github.com/shmootidy" target="_blank" className="contact-bar" id="github">
+        <a href="https://www.github.com/shmootidy" target="_blank" rel="noopener noreferrer" className="contact-bar" id="github">
           <img className="contact-icon absolute" alt="Github icon" src={require("./assets/github.png")} />
           <span className="contact-info">> /shmootidy</span>
         </a>
