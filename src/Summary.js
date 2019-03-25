63d3650989d329df10d2fad2c00b1791c88f5b3d
Shmoo <shmooritchie@gmail.com>
2019-03-24 19:46:48 -0700

    Revise content.

--- src/Summary.js
+++ src/Summary.js
@@ -5,9 +5,8 @@ export default class Summary extends Component {
     return (
       <div id="summary">
         <h1 className='content-title'>susannah-ritchie</h1>
-        <p>My real name is Susannah, but everyone calls me Shmoo.</p>
-        <p>I'm a full-stack web developer, passionate about creative problem solving, collaborative work, and honing the craft of writing elegant code.</p>
-        <p>My experience as a teacher, musician, and entrepreneur have honed in me a high level of integrity, cretaivty, and tenacity.</p>
+        <p>Passionate about creative problem solving, collaborative work, and honing the craft of writing elegant code.</p>
+        <p>Self-starter with an eye for detail and high levels of integrity, resourcefulness, and tenacity, from years as a teacher, entrepreneur, and musician.</p>
         <p>Always keen to learn and improve</p>
       </div>
     )
