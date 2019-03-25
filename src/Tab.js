d64bdcb4c73edbd75d7570346e11177f6871d193
Shmoo <shmooritchie@gmail.com>
2019-03-24 20:19:22 -0700

    Adjust specificity of li styling to differentiate between project list items and tabs.

--- src/Tab.js
+++ src/Tab.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Tab = (props) => {
   const tab = props.tabs.map((tab, index) => {
     let selectedClass = tab.active ? '' : 'not-selected'
-    let tabClass = 'tab' + (index + 1).toString()
+    let tabClass = 'tab tab' + (index + 1).toString()
     let fullClass =  tabClass + ' ' + selectedClass
     let tabText = tab.text
     return <li 
