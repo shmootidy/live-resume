500a15e1c04277daa17b6d9746afabc83e29253a
Shmoo <shmooritchie@gmail.com>
2019-03-24 19:39:22 -0700

    Comment out switch case to have all content on one page again.

--- src/Content.js
+++ src/Content.js
@@ -11,16 +11,24 @@ import './style/content.css'
 const Content = (props) => {
   let activeTab = props.tabs.filter(content => content.active === true)[0].text
 
-  switch(activeTab) {
-    case 'Readme':
-      return <content><Summary /></content>
-    case 'Projects':
-      return <content><Projects /></content>
-    case 'Education':
-      return <content><Education /></content>
-    case 'Experience':
-      return <content><Experience /></content>
-  }
+  return (
+    <content>
+      <Summary />
+      <Projects />
+      <Education />
+      <Experience />
+    </content>
+  )
+  // switch(activeTab) {
+  //   case 'Readme':
+  //     return <content><Summary /></content>
+  //   case 'Projects':
+  //     return <content><Projects /></content>
+  //   case 'Education':
+  //     return <content><Education /></content>
+  //   case 'Experience':
+  //     return <content><Experience /></content>
+  // }
 }
 
 export default Content
