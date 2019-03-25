3f469f6f4b9dcc6212ebcc384540f3a73d0559c2
Shmoo <shmooritchie@gmail.com>
2019-03-24 19:19:34 -0700

    Add alt text to icons img tags.

--- src/Contact.js
+++ src/Contact.js
@@ -6,19 +6,19 @@ export default class Contact extends Component {
       <div id="contact">
         <div className="sidebar-subtitle">contact</div>
         <div className="contact-bar" id="phone">
-          <img className="contact-icon absolute" src={require("./assets/phone.png")} />
+          <img className="contact-icon absolute" alt="Phone icon" src={require("./assets/phone.png")} />
           <span className="contact-info">> 604.616.0247</span>
         </div>
         <div className="contact-bar" id="email">
-          <img className="contact-icon absolute" src={require("./assets/email.png")} />
+          <img className="contact-icon absolute" alt="Email icon" src={require("./assets/email.png")} />
           <span className="contact-info">> shmooritchie@gmail.com</span>
         </div>
         <div className="contact-bar" id="linked-in">
-          <img className="contact-icon absolute" src={require("./assets/linked-in.png")} />
+          <img className="contact-icon absolute" alt="Linked-in icon" src={require("./assets/linked-in.png")} />
           <span className="contact-info">> in/shmoo-ritchie</span>
         </div>
         <div className="contact-bar" id="github">
-          <img className="contact-icon absolute" src={require("./assets/github.png")} />
+          <img className="contact-icon absolute" alt="Github icon" src={require("./assets/github.png")} />
           <span className="contact-info">> /shmootidy</span>
         </div>
       </div>
