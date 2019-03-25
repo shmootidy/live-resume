8611f0f94fa009686e41d552d173c44af36d142f
Shmoo <shmooritchie@gmail.com>
2019-03-24 22:55:19 -0700

    Create component for ShmooSearch with functionality to loop through text telling a little about my name.
    Styling needs to be fixed.

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,39 @@
 // contains Random, NameTitleTabs
 
-import React from 'react'
+import React, { Component } from 'react'
 import Random from './Random'
 import NameTitleTabs from './NameTitleTabs'
+import ShmooSearch from './ShmooSearch'
 
 import './style/header.css'
 
-const Header = (props) => {
-  return (
+export default class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      iteration: 0,
+      message: ['What is a Shmoo anyway?', 'Shmoo is my name! Well...my real name is Susannah...', '\'Shmoo\' comes from the comic, Lil Abner.', 'My parents gave me the nickname as a baby, and it stuck.']
+    }
+    this.loopMessage = this.loopMessage.bind(this)
+  }
+
+  loopMessage() {
+    let iteration = this.state.iteration + 1
+    this.setState({ iteration })
+  }
+
+  render() {
+    return (
     <div id="header" className="sticky">
       <Random />
-      <div className="header-section">
-        <span id="shmoo">shmoo</span>
-        <span>What is a Shmoo anyway?</span>
+      <div className="header-section flex">
+        <span><span id="shmoo">shmoo</span></span>
+        {/* <img id="shmoo-img" src={require("./assets/shmoo.jpg")} /> */}
+        <ShmooSearch data={this.state} loopMessage={this.loopMessage}/>
       </div>
-      <NameTitleTabs tabs={props.tabs} tabToggle={props.tabToggle} />
+      <NameTitleTabs tabs={this.props.tabs} tabToggle={this.props.tabToggle} />
     </div>
-  )
+    )
+  }
 }
-
-export default Header
+// http://lil-abner.com/the-shmoo/
