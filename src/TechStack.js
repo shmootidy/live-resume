b5be4c9e230880f324e7682b522697e2ee6f2a82
Shmoo <shmooritchie@gmail.com>
2019-03-26 21:15:17 -0700

    Render and style skills. These still need revision.

--- src/TechStack.js
+++ src/TechStack.js
@@ -9,21 +9,3 @@ const TechStack = (props) => {
 }
 
 export default TechStack
-
-/* HOLDING - will likely use this for skills in SideBar 
-
-  let key = 0;
-  let techStack = Object.entries(props.techStack).map(tech => {
-    key++
-    let techValueColor = key % 2 === 0 ? 'red' : 'green'
-    let techValueClass = 'tech tech-value-' + techValueColor
-
-    return (
-      <span className="project-tech" key={key}>
-        <span className="tech-key tech">{tech[0]}</span>
-        <span className={techValueClass}>{tech[1]}</span>
-      </span>
-    )
-})
-  return techStack
-  */
