b3e77f7eeee5640e2e0ac5539d24f5ba211814f7
Shmoo <shmooritchie@gmail.com>
2019-03-26 22:53:46 -0700

    Edit summary text.

--- src/Summary.js
+++ src/Summary.js
@@ -4,7 +4,7 @@ const Summary = (props) => {
   return (
     <div id="summary">
       <h1 className='content-title'>shmoo-ritchie</h1>
-      <p>Passionate about development, design, and writing clean code. Integrity, resourcefulness, and creativity honed from experience as a teacher, musician, and entrepreneur. A driven self-starter who enjoys working collaboratively and is always keen to learn and improve.</p>
+      <p>Passionate about development, design, and writing <span className="highlight">DRY code</span>. Tenacity, resourcefulness, and creativity honed from experience as a teacher, musician, and entrepreneur. A driven self-starter who enjoys working collaboratively and is always keen to learn and improve.</p>
     </div>
   )
 }
