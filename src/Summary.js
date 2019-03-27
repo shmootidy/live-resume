bda64e8f6e2d772ce7fdc57dce19b1fca88dd94d
Shmoo <shmooritchie@gmail.com>
2019-03-26 19:51:30 -0700

    Fix text typo.

--- src/Summary.js
+++ src/Summary.js
@@ -6,7 +6,7 @@ const Summary = (props) => {
       <h1 className='content-title'>shmoo-ritchie</h1>
       <p>Passionate about creative problem solving, collaborative work, and honing the craft of writing elegant code.</p>
       <p>Driven self-starter with an eye for detail and high levels of integrity, resourcefulness, and tenacity, from years as a teacher, entrepreneur, and musician.</p>
-      <p>Always keen to learn and improve</p>
+      <p>Always keen to learn and improve.</p>
     </div>
   )
 }
