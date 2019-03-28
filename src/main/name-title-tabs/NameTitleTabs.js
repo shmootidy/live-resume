f67892d19f650ea8e2790df225787d0b1e26762c
Shmoo <shmooritchie@gmail.com>
2019-03-27 22:22:39 -0700

    Adjust padding for NTT elements at breakpoint.

--- src/main/name-title-tabs/NameTitleTabs.js
+++ src/main/name-title-tabs/NameTitleTabs.js
@@ -8,8 +8,10 @@ import './name-title-tabs.css'
 const NameTitleTabs = (props) => {
   return (
     <div id="name-title-tabs" className="flex flex-column">
-      <span id="name">susannah-ritchie</span>
-      <span id="title">Full-stack <span className="green">Web</span> Developer • Vancouver, BC</span>
+      <span  className="responsive-padding flex flex-column">
+        <span id="name">susannah-ritchie</span>
+        <span id="title">Full-stack <span className="green">Web</span> Developer • Vancouver, BC</span>
+      </span>
       <Tabs tabs={props.tabs} tabToggle={props.tabToggle} scrollToContent={props.scrollToContent} />
     </div>
   )
