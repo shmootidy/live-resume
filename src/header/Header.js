3946bb33b09c9c9bd81b2e178c5c5f0c29af5410
Shmoo <shmooritchie@gmail.com>
2019-03-27 18:29:53 -0700

    Create new component for ShmooSearch.

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,36 +3,18 @@
 import React, { Component } from 'react'
 import Random from './Random'
 import NameTitleTabs from './NameTitleTabs'
+import ShmooSearch from './ShmooSearch';
 
 import '../style/header.css'
 
 export default class Header extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      iteration: 0,
-      message: ['What is a Shmoo anyway?', 'Shmoo is my name! Well...my real name is Susannah...', '\'Shmoo\' comes from the comic, Lil Abner.', 'My parents gave me the nickname as a baby, and it stuck.', 'Click here to learn more about the Shmoo!']
-    }
-    this.loopMessage = this.loopMessage.bind(this)
-  }
 
-  loopMessage() {
-    let iteration = this.state.iteration + 1
-    let maxLoop = this.state.message.length - 1
-    if (iteration > maxLoop) iteration = 0
-    this.setState({ iteration })
-  }
 
   render() {
     return (
     <div id="header" className="sticky">
       <Random />
-      <div className="header-section flex">
-        <span><span id="shmoo-npm">shmoo</span></span>
-        <a href="https://en.wikipedia.org/wiki/Shmoo" target="_blank" rel="noopener noreferrer" id="shmoo-search">{this.state.message[this.state.iteration]}</a>
-        <div id="shmoo-button" onClick={this.loopMessage}>Click</div>
-        {/* <img id="shmoo-img" src={require("./assets/shmoo.jpg")} /> */}
-      </div>
+      <ShmooSearch />
       <NameTitleTabs tabs={this.props.tabs} tabToggle={this.props.tabToggle} scrollToContent={this.props.scrollToContent} />
     </div>
     )
---
+++ src/header/ShmooSearch.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class ShmooSearch extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      iteration: 0,
+      message: ['What is a Shmoo anyway?', 'Shmoo is my name! Well...my real name is Susannah...', '\'Shmoo\' comes from the comic, Lil Abner.', 'My parents gave me the nickname as a baby, and it stuck.', 'Click here to learn more about the Shmoo!']
+    }
+    this.loopMessage = this.loopMessage.bind(this)
+  }
+
+  loopMessage() {
+    let iteration = this.state.iteration + 1
+    let maxLoop = this.state.message.length - 1
+    if (iteration > maxLoop) iteration = 0
+    this.setState({ iteration })
+  }
+   render() {
+    return (
+    <div className="header-section flex">
+      <span><span id="shmoo-npm">shmoo</span></span>
+      <a href="https://en.wikipedia.org/wiki/Shmoo" target="_blank" rel="noopener noreferrer" id="shmoo-search">{this.state.message[this.state.iteration]}</a>
+      <div id="shmoo-button" onClick={this.loopMessage}>Click</div>
+      {/* <img id="shmoo-img" src={require("./assets/shmoo.jpg")} /> */}
+    </div>
+    )
+  }
+}
