c623fff5095e8b5796a500e5cad6daa61711a1a2
Shmoo <shmooritchie@gmail.com>
2019-03-27 22:52:51 -0700

    Remove unneeded div.

--- src/main/name-title-tabs/NameTitle.js
+++ src/main/name-title-tabs/NameTitle.js
@@ -7,12 +7,10 @@ import './name-title-tabs.css'
 
 const NameTitleTabs = (props) => {
   return (
-    <div id="name-title-tabs" className="flex flex-column">
-      <span id="name-title" className="flex flex-column responsive-padding sticky">
-        <span id="name">susannah-ritchie</span>
-        <span id="title">Full-stack <span className="green">Web</span> Developer • Vancouver, BC</span>
-      </span>
-    </div>
+    <span id="name-title" className="flex flex-column responsive-padding sticky">
+      <span id="name">susannah-ritchie</span>
+      <span id="title">Full-stack <span className="green">Web</span> Developer • Vancouver, BC</span>
+    </span>
   )
 }
 
