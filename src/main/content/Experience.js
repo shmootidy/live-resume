7751e87f6ebb005a484b5f90901365ef6b5f53cf
Shmoo <shmooritchie@gmail.com>
2019-03-28 23:07:44 -0700

    Add details to experience.

--- src/main/content/Experience.js
+++ src/main/content/Experience.js
@@ -3,29 +3,29 @@ import React, { Component } from 'react'
 
 const Experience = (props) => {
   const jobs = [
-    { 
-      title: 'Bartender',
-      employer: 'The Emerald Lounge',
-      duration: 'Mar \'18 - Dec \'18',
-      skills: ['Solid Team Player']
-    },
+    // { 
+    //   title: 'Bartender',
+    //   employer: 'The Emerald Lounge',
+    //   duration: 'Mar \'18 - Dec \'18',
+    //   skills: ['Served cocktails with charm, accuracy, and speed.', '']
+    // },
     {
       title: 'Company Owner',
       employer: 'Lionheart Sleep School',
       duration: 'Jul \'16 - Nov \'18',
-      skills: ['Brand Development']
+      skills: ['Helped anxious new parents improve their baby\'s sleep.', 'Developed consistent branding across all platforms.', 'Organized community outreach events.']
     },
     {
       title: 'High School Teacher',
       employer: 'Avenir School Society',
       duration: 'Feb \'14 - Jun \'15',
-      skills: ['Managing Long-Term, Complex Projects']
+      skills: ['Adapted curriculum to meet diverse student needs.', 'Managed school-wide programs, such as daily hot lunches, field trips, and meditation breaks.']
     },
     {
       title: 'Musician',
       employer: 'The Organ / Keep Tidy',
       duration: 'Nov \'04 - Sep \'12',
-      skills: ['Live Performance']
+      skills: ['Performed 100s of live shows in venues worldwide.', 'Wrote and recorded original music.']
     }
   ]
 
