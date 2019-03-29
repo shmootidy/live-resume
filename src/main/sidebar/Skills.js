1c252c4f0ee6683176aa0ad8b76ec51bc93a165a
Shmoo <shmooritchie@gmail.com>
2019-03-29 12:48:17 -0700

    Render skills on page. Revise shape of web technologies to prevent error. Sidebar isn't sticky...wtf?

--- src/main/sidebar/Skills.js
+++ src/main/sidebar/Skills.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
 
 const Skills = (props) => {
-  let skills = 'woof'
+  let skills = Object.entries(props.skills).map(skill => {
+    return (
+      <div>
+        <div>{skill[0]}</div>
+        <div>
+          {skill[1].map(skillDetail => <div>{skillDetail}</div>)}
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div id="skills">
