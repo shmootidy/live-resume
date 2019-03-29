ff4faf791cdb030dec3c09ecf07a2ea3b194d239
Shmoo <shmooritchie@gmail.com>
2019-03-28 22:23:43 -0700

    Fill out Education component.

--- src/main/content/Education.js
+++ src/main/content/Education.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Education extends Component {
-  render() {
+const Education = (props) => {
+  const educationArr = [
+    {
+      school: 'Lighthouse Labs',
+      awarded: 'Diploma of Web Development',
+      year: '2019'
+    },
+    {
+      school: 'UBC',
+      awarded: 'Bachelor of Education',
+      year: '2012'
+    },
+    {
+      school: 'UBC',
+      awarded: 'Bachelor of Arts',
+      year: '2011'
+    }
+  ]
+
+  let education = educationArr.map(education => {
     return (
-      <div id="education">
-        <h2 className="content-title">Education</h2>
+      <div>
+        <div className="awarded description-key">{education.awarded.toUpperCase()}</div>
+        <span className="flex space-between project-subtitle">
+          <span className="school">{education.school}</span>
+          <span className="year">{education.year}</span>
+        </span>
       </div>
     )
-  }
+  })
+
+  return (
+    <div id="education">
+      <h2 className="content-title">Education</h2>
+      <div>{education}</div>
+    </div>
+  )
 }
+
+export default Education
