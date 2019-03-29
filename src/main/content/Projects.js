4175a3f0683afef68a7d381c69782658c4ee3fa9
Shmoo <shmooritchie@gmail.com>
2019-03-28 21:49:48 -0700

    Comment out Chatty and add this resume to list of projects. (It uses the same stack anyways.) Add recursive image.

--- src/main/content/Projects.js
+++ src/main/content/Projects.js
@@ -4,12 +4,29 @@ import './projects.css'
 
 const Projects = (props) => {
   const projects = [
+    {
+      title: 'This Resume',
+      subtitle: 'Professional Document',
+      dateCompleted: 'Mar 2019',
+      description: {
+        design: ['Mimicked the style of npmjs.com through close study.'],
+        development: ['Adapted npm\'s layout to emphasize pertinent resume information.'],
+        workflow: ['Frequent, clear commit messages.', 'On-the-go code clean up and refactoring.'],
+      },
+      techStack: {
+        framework: 'react.js',
+        styling: 'CSS',
+          hosting: 'github',
+      },
+      url: 'https://www.npmjs.com/package/react',
+      img: 'resume.png'
+    },
     { 
       title: 'Don\'t Panic',
       subtitle: 'Life Skills Education Game',
       dateCompleted: 'Mar 2019',
       description: {
-        leadership: ['Led a team of 3 developers from conception to presention in 13 days.'],
+        teamwork: ['Led a team of 3 developers from conception to presention in 13 days.', 'Assigned tasks and managed workflow.'],
         // teamwork: ['collaboratively developed the concept, gameplay, design, and construction of the game'],
         workflow: ['Fixed repository conflicts.', 'Refactored code to maximize reusability and simplify debugging.'],
         'design & development': ['Conceptualized and implemented the scoring logic.', 'Integrated disparate features.']
@@ -25,20 +42,22 @@ const Projects = (props) => {
       img: 'dont-panic.gif',
       award: true,
     },
-    {
-      title: 'Chatty App',
-      subtitle: 'Real-time Chat App',
-      dateCompleted: 'Feb 2019',
-      description: {beep:['Set up local server', 'Style elements', 'Modularize data structure']},
-      techStack: {
-        framework: 'react.js',
-        API: 'websocket'
-      },
-      // techStack: ['React', 'Websocket'],
-        // there's more! what about the API?
-      url: 'https://github.com/shmootidy/Chatty',
-      img: 'chatty.jpg'
-    },
+    // {
+    //   title: 'Chatty App',
+    //   subtitle: 'Real-time Chat App',
+    //   dateCompleted: 'Feb 2019',
+    //   description: {
+    //     beep:['Set up local server', 'Style elements', 'Modularize data structure']
+    //   },
+    //   techStack: {
+    //     framework: 'react.js',
+    //     API: 'websocket'
+    //   },
+    //   // techStack: ['React', 'Websocket'],
+    //     // there's more! what about the API?
+    //   url: 'https://github.com/shmootidy/Chatty',
+    //   img: 'chatty.jpg'
+    // },
     {
       title: 'Smart Cat',
       subtitle: 'Auto-Categorizing To-Do List',
