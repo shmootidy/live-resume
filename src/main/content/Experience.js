f913fad543077824f09268e4f3593cea3c428152
Shmoo <shmooritchie@gmail.com>
2019-03-28 22:29:48 -0700

    Convert Experience component to functional component and render all elements in jobs obj on page.

--- src/main/content/Experience.js
+++ src/main/content/Experience.js
@@ -1,47 +1,51 @@
 import React, { Component } from 'react'
-import Job from './Job'
+// import Job from './Job'
 
-export default class Experience extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      jobs: [
-        { 
-          title: 'Bartender',
-          employer: 'The Emerald Lounge',
-          duration: 'Mar \'18 - Dec \'18',
-          skills: 'Solid Team Player'
-        },
-        {
-          title: 'Company Owner',
-          employer: 'Lionheart Sleep School',
-          duration: 'Jul \'16 - Nov \'18',
-          skills: 'Brand Development'
-        },
-        {
-          title: 'High School Teacher',
-          employer: 'Avenir School Society',
-          duration: 'Feb \'14 - Jun \'15',
-          skills: 'Managing Long-Term, Complex Projects'
-        },
-        {
-          title: 'Musician',
-          employer: 'The Organ / Keep Tidy',
-          duration: 'Nov \'04 - Sep \'12',
-          skills: 'Live Performance'
-        }
-      ]
+const Experience = (props) => {
+  const jobs = [
+    { 
+      title: 'Bartender',
+      employer: 'The Emerald Lounge',
+      duration: 'Mar \'18 - Dec \'18',
+      skills: 'Solid Team Player'
+    },
+    {
+      title: 'Company Owner',
+      employer: 'Lionheart Sleep School',
+      duration: 'Jul \'16 - Nov \'18',
+      skills: 'Brand Development'
+    },
+    {
+      title: 'High School Teacher',
+      employer: 'Avenir School Society',
+      duration: 'Feb \'14 - Jun \'15',
+      skills: 'Managing Long-Term, Complex Projects'
+    },
+    {
+      title: 'Musician',
+      employer: 'The Organ / Keep Tidy',
+      duration: 'Nov \'04 - Sep \'12',
+      skills: 'Live Performance'
     }
-  }
-  render() {
-    const jobs = this.state.jobs.map(job => {
-      return <Job job={job} key={job.title} />
-    })
+  ]
+
+  const job = jobs.map(job => {
     return (
-      <div id="experience">
-        <h2 className="content-title">Experience</h2>
-        {jobs}
+      <div>
+        <div className="job-title">{job.title}</div>
+        <div className="job-employer">{job.employer}</div>
+        <div className="job-duration">{job.duration}</div>
+        <div className="job-skill">{job.skills}</div>
       </div>
     )
-  }
+  })
+
+  return (
+    <div id="experience">
+      <h2 className="content-title">Experience</h2>
+      <div>{job}</div>
+    </div>
+  )
 }
+
+export default Experience
