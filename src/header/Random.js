e36c123c66b1e5ee3d5446ada4e12030de43a3d3
Shmoo <shmooritchie@gmail.com>
2020-07-31 13:00:11 -0700

    Create qualities to display randomizer for mobile (and dt) header display.

--- src/header/Random.js
+++ src/header/Random.js
@@ -5,11 +5,27 @@ import React, { Component } from 'react'
 import './random.scss'
 
 export default class Random extends Component {
+  displayQualities() {
+    const randomQualities = ['Concise', 'Reliable', 'Approachable', 'Creative', 'Resourceful', 'Knowledgeable']
+      .map(a => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(a => a.value)
+    return randomQualities.map((quality, i) => {
+      if (i < 4) {
+        return <span>{quality}</span>
+      }
+    })
+  }
+
   render() {
+    const qualitiesToDisplay = this.displayQualities()
     return (
       <div id="random">
-        <span id="heart">❤</span>
-        <span id="random-msg">Messy Hair • Tidy Code</span>
+        <div>
+          <span id="heart">❤</span>
+          <span id="random-msg">Messy Hair • Tidy Code</span>
+        </div>
+        <div>{ qualitiesToDisplay }</div>
       </div>
     )
   }
