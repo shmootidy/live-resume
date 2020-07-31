79ab5090f5c142bf9d886a99a21a930e042c6596
Shmoo <shmooritchie@gmail.com>
2020-07-31 15:36:39 -0700

    Tweak tabs and random styles.

--- src/main/name-title-tabs/NameTitle.js
+++ src/main/name-title-tabs/NameTitle.js
@@ -5,7 +5,7 @@ import './name-title-tabs.scss'
 const NameTitle = (props) => {
   return (
     <span className="flex flex-column responsive-padding sticky name-title ntt">
-      <span id="name">susannah-ritchie</span>
+      <span id="name">susannah ritchie (shmoo)</span>
       <span id="title">1.4.3 • <span className="green">Web Developer</span> • Vancouver, BC</span>
     </span>
   )
