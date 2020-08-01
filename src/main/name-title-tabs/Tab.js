2c627ba0780e011118c7c45012b543fbaa34f5a6
Shmoo <shmooritchie@gmail.com>
2020-07-31 20:52:14 -0700

    Convert tabs into class. Add stickiness and top value.

--- src/main/name-title-tabs/Tab.js
+++ src/main/name-title-tabs/Tab.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Tab = (props) => {
-  const tab = props.tabs.map((tab, index) => {
-    let selectedClass = tab.active ? '' : 'not-selected'
-    let tabClass = 'tab tab' + (index + 1).toString()
-    let fullClass =  tabClass + ' ' + selectedClass
-    let tabText = tab.text
-    return <li
-        style={props.style}
-        key={tab.text}
-        className={fullClass + ' sticky'} 
-        onClick={() => { props.tabToggle(index); props.scrollToContent(index) }}>
-        {tabText}
-      </li>
-  })
-  return tab
+export default class Tab extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const tab = this.props.tabs.map((tab, index) => {
+      let selectedClass = tab.active ? '' : 'not-selected'
+      let tabClass = 'tab tab' + (index + 1).toString()
+      let fullClass =  tabClass + ' ' + selectedClass
+      let tabText = tab.text
+      return <li
+          style={this.props.style}
+          key={tab.text}
+          className={fullClass + ' sticky'} 
+          onClick={() => { this.props.tabToggle(index); this.props.scrollToContent(index) }}>
+          {tabText}
+        </li>
+    })
+    return tab
+  }
 }
-
-export default Tab
