4e526be760859d86efe428d900d8852083a07aee
Shmoo <shmooritchie@gmail.com>
2020-08-02 18:04:06 -0700

    Revert tab to fucntional component.

--- src/main/name-title-tabs/Tab.js
+++ src/main/name-title-tabs/Tab.js
@@ -1,22 +1,20 @@
 import React, { Component, useEffect } from 'react'
 
-export default class Tab extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    const tab = this.props.tabs.map((tab, index) => {
-      let selectedClass = tab.active ? '' : 'not-selected'
-      let tabClass = 'tab tab' + (index + 1).toString()
-      let fullClass =  tabClass + ' ' + selectedClass
-      let tabText = this.props.tabMobile ? tab.icon : tab.text
-      return <li
-          key={tab.text}
-          className={fullClass} 
-          onClick={() => { this.props.tabToggle(index); this.props.scrollToContent(index) }}>
-          {tabText}
-        </li>
-    })
-    return tab
-  }
+ const Tab = (props) => {
+// useEffect
+  const tab = props.tabs.map((tab, index) => {
+    let selectedClass = tab.active ? '' : 'not-selected'
+    let tabClass = 'tab tab' + (index + 1).toString()
+    let fullClass =  tabClass + ' ' + selectedClass
+    let tabText = props.tabMobile ? tab.icon : tab.text
+    return <li
+        key={tab.text}
+        className={fullClass} 
+        onClick={() => { props.tabToggle(index); props.scrollToContent(index) }}>
+        {tabText}
+      </li>
+  })
+  return tab
 }
+
+export default Tab
