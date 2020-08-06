fa0f9059fcc3b9d3c5d9c8c394654064f801de49
Shmoo <shmooritchie@gmail.com>
2020-08-06 16:38:54 -0700

    Toggle see more text, at least in the console. Gotta review how states work in functional components.

--- src/main/sidebar/Contact.js
+++ src/main/sidebar/Contact.js
@@ -8,39 +8,48 @@ const Contact = (props) => {
     margin: '.5rem 0 1rem',
     color: 'rgba(0,0,0,.8)'
   }
+  let seeMoreToggle = false
+  let seeMoreText = '[ see more ]'
   
+  function handleToggle() {
+    seeMoreText = seeMoreToggle ? '[ see more ]' : '[ see less ]'
+    seeMoreToggle = seeMoreToggle ? false : true
+    console.log(seeMoreText)
+  }
   return (
     <div className="sidebar">
       <div className="contact">
-        <div className="sidebar-subtitle">Contact</div>
+        <div className="sidebar-subtitle">Contact <button onClick={handleToggle} style={{border: 0, background: 'white', color: 'inherit'}} className="see-more">{ seeMoreText }</button></div>
         <a href="mailto:shmooritchie@gmail.com?subject=I saw your resume!" className="contact-bar" id="email">
           <img className="contact-icon absolute" alt="Email icon" src={require("../../assets/email.png")} />
           <span className="contact-info">> shmooritchie@gmail.com</span>
         </a>
+      </div>
+      <div className="contact collapseable-contact" style={{display: 'none', overflow: 'hidden'}}>
         <a href="tel:+1-604-616-0247" className="contact-bar" id="phone">
           <img className="contact-icon absolute" alt="Phone icon" src={require("../../assets/phone.png")} />
           <span className="contact-info">> 604.616.0247</span>
         </a>
-      </div>
-      <div style={{marginLeft: '.25rem'}}>
-        <div style={{borderBottom: '1px solid #e1e1e1'}}>
-          <div className="sidebar-subtitle">Linked In</div>
-          <a style={style} href="https://www.linkedin.com/in/shmoo-ritchie" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={['fab', 'linkedin']} />
-            <span style={{marginLeft: '.5rem'}}>linkedin.com/in/shmoo-ritchie</span>
-          </a>
-        </div>
-        <div style={{borderBottom: '1px solid #e1e1e1'}}>
-          <div className="sidebar-subtitle">Repository</div>
-          <a style={style} href="https://www.github.com/shmootidy" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={['fab', 'github']} />
-            <span style={{marginLeft: '.5rem'}}>github.com/shmootidy</span>
-          </a>
-        </div>
-        <div>
-          <div className="sidebar-subtitle">Collaborators</div>
-          <div style={style}>
-            <img style={{width: '42px', height: '42px', minWidth: '42px', borderRadius: '4%'}} alt="avatar" src={require('../../assets/headshot.png')} />
+        <div style={{marginLeft: '.25rem'}}>
+          <div style={{borderBottom: '1px solid #e1e1e1'}}>
+            <div className="sidebar-subtitle">Linked In</div>
+            <a style={style} href="https://www.linkedin.com/in/shmoo-ritchie" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={['fab', 'linkedin']} />
+              <span style={{marginLeft: '.5rem'}}>linkedin.com/in/shmoo-ritchie</span>
+            </a>
+          </div>
+          <div style={{borderBottom: '1px solid #e1e1e1'}}>
+            <div className="sidebar-subtitle">Repository</div>
+            <a style={style} href="https://www.github.com/shmootidy" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={['fab', 'github']} />
+              <span style={{marginLeft: '.5rem'}}>github.com/shmootidy</span>
+            </a>
+          </div>
+          <div>
+            <div className="sidebar-subtitle">Collaborators</div>
+            <div style={style}>
+              <img style={{width: '42px', height: '42px', minWidth: '42px', borderRadius: '4%'}} alt="avatar" src={require('../../assets/headshot.png')} />
+            </div>
           </div>
         </div>
       </div>
