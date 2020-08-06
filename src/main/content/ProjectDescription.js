df115f0da52f86026d0478521a91185f4bbb7f3e
Shmoo <shmooritchie@gmail.com>
2020-08-06 13:03:06 -0700

    Tweak

--- src/main/content/ProjectDescription.js
+++ src/main/content/ProjectDescription.js
@@ -7,7 +7,7 @@ const ProjectDescription = (props) => {
     let note = value.map(note => <div className="description-value">{note}</div>)
     return (
       <div className="description">
-        <div className="description-key">{key}</div>
+        <div className="description-key">{key.replace(key.substring(0,1), key.substring(0,1).toUpperCase())}</div>
         <span>{note}</span>
       </div>
     )
