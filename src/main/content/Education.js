a48a7ea80c634d833af0db859005ae1df1ad9caf
Shmoo <shmooritchie@gmail.com>
2020-08-12 12:23:10 -0700

    Elaborate on education. Tweak styles.

--- src/main/content/Education.js
+++ src/main/content/Education.js
@@ -5,28 +5,32 @@ const Education = (props) => {
     {
       school: 'Lighthouse Labs',
       awarded: 'Diploma of Web Development',
-      year: '2019'
+      year: '2019',
+      skills: ['Led team of new developers in complex project with new tools under tight deadline', 'Awarded Best Final Project', 'Became adept at various programming languages', 'Learned web development fundamentals']
     },
     {
       school: 'UBC',
       awarded: 'Bachelor of Education',
-      year: '2012'
+      year: '2012',
+      skills: ['Learned pedagogical models', 'Improved collaborative skills', 'Performed and facilitated hours of public speaking and group engagement']
     },
     {
       school: 'UBC',
       awarded: 'Bachelor of Arts',
-      year: '2011'
+      year: '2011',
+      skills: ['Honed critical thinking and communication skills', 'Broadened my historical and cultural understanding', 'Developed disciplined, independent working schedule']
     }
   ]
 
   let education = educationArr.map((education, i) => {
     return (
-      <div key={i}>
+      <div className="education" key={i}>
         <h3 style={{margin: '1rem 0 0'}} className="awarded">{education.awarded.toUpperCase()}</h3>
         <span style={{margin: 0}} className="flex space-between project-subtitle">
           <span className="school">{education.school}</span>
           <span className="year">{education.year}</span>
-        </span>
+          </span>
+        <ul>{education.skills.map((skill, j) => <li key={j}>{skill}</li> )}</ul>
       </div>
     )
   })
