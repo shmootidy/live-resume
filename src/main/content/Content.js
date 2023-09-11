da201cb138ece300a4d92cf5d3902c900360da73
Shmoo <shmooritchie@gmail.com>
2023-09-10 17:10:53 -0700

    Update summary section: comment out skills and revise opening paragraphs; write silly function for contact buttons

--- src/main/content/Content.js
+++ src/main/content/Content.js
@@ -37,7 +37,7 @@ const Content = (props) => {
         />
         <div className="content">
           <Summary />
-          <Skills />
+          {/* <Skills /> */}
         </div>
       </content>
     )
--- src/main/content/Summary.js
+++ src/main/content/Summary.js
@@ -5,7 +5,6 @@ const Summary = (props) => {
   return (
     <div>
       <div id="summary">
-        {/* <h1 className='content-title'>shmoo-ritchie</h1> */}
         <h1 style={{
           margin: 0,
           borderBottom: '1px solid #ececec',
@@ -19,10 +18,48 @@ const Summary = (props) => {
           }}
           className='highlight'>shmoo</code>
         </h1>
-        <p>Shmoo is a full-stack Intermediate Software Developer with 3+ years of professional experience. She has an eye for good design and interfaces.</p>
+        <p>Shmoo is a full-stack Intermediate Software Developer with 3+ years of professional experience. She has an eye for good design and interfaces, dubbed the "CSS Wizard" of her team.</p>
         <p>The <span className="highlight">shmoo</span> package contains high standards for quality code, a good attitude, and a track record of developing quality custom end-to-end features.</p>
         <p>Shmoo has a well-rounded background in customer service and education, bringing empathy, energy, and communication skills to your tech team.</p>
-        <p><a href="mailto:shmooritchie@gmail.com?subject=I saw your resume!"><span className="highlight">Get in touch!</span></a></p>
+        {/* <p><a href="mailto:shmooritchie@gmail.com?subject=I saw your resume!"><span className="highlight">Get in touch!</span></a></p> */}
+      </div>
+      <div>
+        <h2 style={{
+          margin: 0,
+          borderBottom: '1px solid #ececec',
+        }}>Usage</h2>
+        <div style={{
+          margin: '16px 0',
+          fontSize: 16,
+            background: 'rgb(247, 247, 247)',
+            borderRadius: 2,
+            padding: '13px 15px',
+          }}>
+          <code>
+            <div style={{ marginBottom: 16 }}>
+              <span style={{ color: 'rgb(215, 58, 73)' }}>import</span>
+              {` { shmoosNumber, shmoosEmail } `}
+              <span style={{ color: 'rgb(215, 58, 73)' }}>from</span>
+              {` 'shmoos-live-resume'`}
+            </div>
+            <div>
+              <span style={{ color: 'rgb(215, 58, 73)' }}>{`function `}</span>
+              <span style={{ color: 'rgb(227, 98, 9)'  }}>contactShmoo</span>
+              <span>{`() {`}</span>
+              <div style={{
+                margin: 16,
+                display: 'flex',
+                justifyContent: 'space-evenly',
+              }}>
+                <a href="mailto:shmooritchie@gmail.com?subject=I saw your resume!" className='contact-shmoo-button'>
+                  Click to email Shmoo
+                </a>
+                <a href="tel:+1-604-616-0247" className='contact-shmoo-button'>Click to call Shmoo</a>
+              </div>
+              <div>{`}`}</div>
+            </div>
+          </code>
+        </div>
       </div>
     </div>
   )
