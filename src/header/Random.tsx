1e8d0fe01e4b3a56d4fba0fb623cc9ce63a78148
shmoo <shmooritchie@gmail.com>
2025-02-02 14:13:33 -0800

    delete some stylesheets and move styles inline, for header components

--- src/header/Random.tsx
+++ src/header/Random.tsx
@@ -1,30 +1,68 @@
 // contains random faux-acronym and heart
+import React from "react";
 
-import React, { Component } from 'react'
+export default function Random() {
+  const randomQualities = [
+    "Thorough",
+    "Proactive",
+    "Concise",
+    "Reliable",
+    "Approachable",
+    "Creative",
+    "Resourceful",
+    "Knowledgeable",
+  ]
+    .map((a) => ({ sort: Math.random(), value: a }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value);
 
-import './random.scss'
-
-export default class Random extends Component {
-  displayQualities() {
-    const randomQualities = ['Thorough', 'Proactive', 'Concise', 'Reliable', 'Approachable', 'Creative', 'Resourceful', 'Knowledgeable']
-      .map(a => ({ sort: Math.random(), value: a }))
-      .sort((a, b) => a.sort - b.sort)
-      .map(a => a.value)
-    return randomQualities.map((quality, i) => {
-      return i < 4 ? <span key={ quality }>{ quality }</span> : null
-    })
-  }
-
-  render() {
-    const qualitiesToDisplay = this.displayQualities()
-    return (
-      <div id="random">
-        <div>
-          <span id="heart">❤</span>
-          <span id="random-msg">Messy Hair • Tidy Code</span>
-        </div>
-        <div>{ qualitiesToDisplay }</div>
+  return (
+    <div
+      id="random"
+      style={{
+        fontFamily: "Poppins",
+        display: "flex",
+        padding: "10px 2rem",
+        justifyContent: "space-between",
+        alignItems: "center",
+        fontSize: "14px",
+      }}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <span
+          id="heart"
+          style={{
+            fontSize: 20,
+            paddingRight: 16,
+            fontWeight: 600,
+          }}
+        >
+          ❤
+        </span>
+        <span
+          id="random-msg"
+          style={{
+            marginRight: "2rem",
+            fontWeight: 100,
+            letterSpacing: 0.21875,
+          }}
+        >
+          Messy Hair • Tidy Code
+        </span>
       </div>
-    )
-  }
+      <div
+        style={{
+          fontWeight: 600,
+        }}
+      >
+        {randomQualities.map((q, i) => {
+          return i < 4 ? (
+            <span style={{ marginLeft: 32 }} key={i}>
+              {q}
+            </span>
+          ) : null;
+        })}
+      </div>
+    </div>
+  );
 }
--- src/header/ShmooSearch.tsx
+++ src/header/ShmooSearch.tsx
@@ -1,34 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 
-// export default class ShmooSearch extends Component {
 export default function ShmooSearch() {
-  // const []
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     iteration: 0,
-  //     // message: ['What is a Shmoo anyway?', 'Shmoo is my name! Well...my real name is Susannah...', '\'Shmoo\' comes from the comic, Lil Abner.', 'My parents gave me the nickname as a baby, and it stuck.', 'Click here to learn more about the Shmoo!']
-  //     message: [
-  //       "Hi, I'm Shmoo!",
-  //       "It's a nickname...",
-  //       'Susannah is "real".',
-  //       "But I go by Shmoo.",
-  //     ],
-  //   };
-  //   this.loopMessage = this.loopMessage.bind(this);
-  // }
-
-  // loopMessage() {
-  //   let iteration = this.state.iteration + 1;
-  //   let maxLoop = this.state.message.length - 1;
-  //   if (iteration > maxLoop) iteration = 0;
-  //   this.setState({ iteration });
-  // }
-  // render() {
   return (
-    <div className="header-section flex">
-      <span>
-        {/* <span id="shmoo-npm">shmoo</span> */}
+    <div
+      style={{
+        display: "flex",
+        padding: 16,
+        alignItems: "center",
+        fontFamily: "Poppins",
+        fontWeight: 600,
+        borderTop: "1px solid #d2d2d2",
+      }}
+    >
+      <span
+        style={{
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "0 1rem",
+          width: "100%",
+        }}
+      >
         <svg
           viewBox="0 0 1260 250"
           xmlns="http://www.w3.org/2000/svg"
@@ -47,25 +39,17 @@ export default function ShmooSearch() {
             stroke="#f7f7f7"
           ></path>
         </svg>
-        <a href="mailto:shmooritchie@gmail.com?subject=I saw your resume!">
+        <a
+          href="mailto:shmooritchie@gmail.com?subject=I saw your resume!"
+          style={{
+            border: "1px solid #ccc",
+            fontSize: 14,
+            padding: "13px 28px",
+          }}
+        >
           Contact Me
         </a>
       </span>
-      {/* <span> */}
-      {/* <a href="https://en.wikipedia.org/wiki/Shmoo" target="_blank" rel="noopener noreferrer" id="shmoo-search">*/}
-      {/* <div id="shmoo-search">
-            {this.state.message[this.state.iteration]}
-          </div>
-          <div id="shmoo-button" onClick={this.loopMessage}>
-            More
-          </div>
-        </span> */}
     </div>
   );
 }
-// }
-
-// alternate links:
-// http://lil-abner.com/the-shmoo/
-// https://en.wikipedia.org/wiki/Shmoo
-// https://hubpages.com/entertainment/The-History-of-the-Shmoo
