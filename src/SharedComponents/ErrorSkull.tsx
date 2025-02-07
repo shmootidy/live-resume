0029bf612b924cda3e0c9654dd14c5e44b3352d4
shmoo <shmooritchie@gmail.com>
2025-02-07 14:52:55 -0800

    tweak the css

--- src/SharedComponents/ErrorSkull.tsx
+++ src/SharedComponents/ErrorSkull.tsx
@@ -5,10 +5,10 @@ import { faSkullCrossbones } from '@fortawesome/free-solid-svg-icons'
 export default function ErrorSkull() {
   return (
     <ErrorSkullWrapper>
-      <SkullIcon icon={faSkullCrossbones} color='#ffcd3a33' />
-      <SkullIcon icon={faSkullCrossbones} color='#cb383733' />
-      <SkullIcon icon={faSkullCrossbones} color='#c836c333' />
-      <SkullIcon icon={faSkullCrossbones} color='#8956ff33' />
+      <SkullIcon icon={faSkullCrossbones} />
+      <SkullIcon icon={faSkullCrossbones} />
+      <SkullIcon icon={faSkullCrossbones} />
+      <SkullIcon icon={faSkullCrossbones} />
     </ErrorSkullWrapper>
   )
 }
@@ -32,67 +32,76 @@ const SkullIcon = styled(FontAwesomeIcon)`
   position: absolute;
   font-size: 100px;
 
+  --c1: #ffcd3a;
+  --c2: #c836c3;
+  --c3: #cb3837;
+  --c4: #8956ff;
+
   --a1: conic-gradient(
     from 0deg at 50% 67.5%,
-    rgba(0, 0, 0, 1) 0deg 60deg,
+    var(--c1) 0deg 60deg,
     transparent 60deg 360deg
   );
   --a1a: conic-gradient(
     from 0deg at 50% 67.5%,
     transparent 0deg 50deg,
-    rgba(0, 0, 0, 1) 50deg 117deg,
+    var(--c1) 50deg 117deg,
     transparent 117deg 50deg
   );
   --a2: conic-gradient(
     from 0deg at 50% 67.5%,
     transparent 0deg 60deg,
-    rgba(0, 0, 0, 1) 60deg 180deg,
+    var(--c1) 60deg 180deg,
     transparent 180deg 60deg
   );
   --a2a: conic-gradient(
     from 0deg at 50% 67.5%,
     transparent 0deg 117deg,
-    rgba(0, 0, 0, 1) 117deg 243deg,
+    var(--c1) 117deg 243deg,
     transparent 243deg 117deg
   );
   --a3: conic-gradient(
     from 0deg at 50% 67.5%,
     transparent 0deg 180deg,
-    rgba(0, 0, 0, 1) 180deg 300deg,
+    var(--c1) 180deg 300deg,
     transparent 300deg 360deg
   );
   --a3a: conic-gradient(
     from 0deg at 50% 67.5%,
     transparent 0deg 243deg,
-    rgba(0, 0, 0, 1) 243deg 300deg,
+    var(--c1) 243deg 300deg,
     transparent 300deg 360deg
   );
   --a4: conic-gradient(
     from 0deg at 50% 67.5%,
     transparent 0deg 300deg,
-    rgba(0, 0, 0, 1) 300deg 360deg
+    var(--c1) 300deg 360deg
   );
   --a4a: conic-gradient(
     from 50deg at 50% 67.5%,
     transparent 70deg 260deg,
-    rgba(0, 0, 0, 1) 260deg 70deg
+    var(--c1) 260deg 70deg
   );
 
   &:nth-child(1) {
     mask-image: var(--a1);
+    color: var(--c1);
     animation: spin1 4s infinite linear;
   }
   &:nth-child(2) {
     mask-image: var(--a2);
     animation: spin2 4s infinite linear;
+    color: var(--c2);
   }
   &:nth-child(3) {
     mask-image: var(--a3);
     animation: spin3 4s infinite linear;
+    color: var(--c3);
   }
   &:nth-child(4) {
     mask-image: var(--a4);
     animation: spin4 4s infinite linear;
+    color: var(--c4);
   }
 
   @keyframes spin1 {
@@ -120,6 +129,9 @@ const SkullIcon = styled(FontAwesomeIcon)`
     87.5% {
       mask-image: var(--a4a);
     }
+    100% {
+      mask-image: var(--a1);
+    }
   }
   @keyframes spin2 {
     0% {
@@ -145,6 +157,9 @@ const SkullIcon = styled(FontAwesomeIcon)`
     }
     87.5% {
       mask-image: var(--a1a);
+    }
+    100% {
+      mask-image: var(--a2);
     }
   }
   @keyframes spin3 {
@@ -172,6 +187,9 @@ const SkullIcon = styled(FontAwesomeIcon)`
     87.5% {
       mask-image: var(--a2a);
     }
+    100% {
+      mask-image: var(--a3);
+    }
   }
   @keyframes spin4 {
     0% {
@@ -197,6 +215,9 @@ const SkullIcon = styled(FontAwesomeIcon)`
     }
     87.5% {
       mask-image: var(--a3a);
+    }
+    100% {
+      mask-image: var(--a4);
     }
   }
 `
